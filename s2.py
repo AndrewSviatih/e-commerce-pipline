26afcbd5dd5680bae7e9a8176498b424f99c6a8e
aserzhenko <aserzhenko@ozon.ru>
2025-06-29 13:35:40 +0300

    delete unnecessary files

--- s2.py
+++
@@ -1,5 +0,0 @@
-from googledrivedownloader import download_file_from_google_drive as gdd
-
-gdd(file_id='1rWUtT8F0LPApGhcLVmvk_YbsKEZSwHza',
-                                    dest_path='./jars/aws-java-sdk-bundle-1.12.459.jar',
-                                    unzip=False)
